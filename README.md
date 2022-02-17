20e625aaa1ae9eaa779c76bbe602333ecbcaec68
DuogMinhTien <okay7290@gmail.com>
2022-02-18 00:08:09 +0700

    change button copy, display code like macOS, bonus big bug...

--- README.md
+++ README.md
@@ -2,28 +2,28 @@ html-snippets:
 <!DOCTYPE html>
 <html lang="vi">
     <head>
-        <title>[TITLE]</title>
+        <link rel="icon" href="../../icon.png"  type="image/x-icon">
+        <title>[TITLE PAGE]</title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="../../style/content.css">
     </head>
     <body>
-        <h1>[LANGUAGE]</h1>
-        <h2>[TITLE]</h2>
-        
-        <div style="display: block;height: 125px;"></div>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.0/prism.min.js"></script>
+        <h1>[TYPE TUTORIAL]L</h1>
+        <h2>[TITLE PAGE]</h2>
+        <div class="code">
+            <h3>[TITLE CODE]</h3>
+            <pre class="code-block"><code class=" language-[TYPE CODE]"></code></pre>
+        </div>
         <script src="../../script/function.js"></script>
-        <script></script>
+        <script defer src="../../script/prism-[TYPE CODE].js"></script>
     </body>
 </html>
 
 code-snippets:
-<div class="code">
-    <h3>Set Cookie</h3>
-        <pre><code class="language-js"></code></pre>
-        <button class="ClickBTN copy" onclick="Copy()">Copy Code</button>
-</div>
+        <div class="code">
+            <h3>[TITLE CODE]</h3>
+            <pre class="code-block"><code class=" language-[TYPE CODE]"></code></pre>
+        </div>
 
-tab: &emsp;&emsp;
+tab: [tab_js]
--- script/function.js
+++ script/function.js
@@ -1,13 +1,55 @@
+const $$ = document.querySelectorAll.bind (document);
+
 document.querySelector ("body").insertAdjacentHTML ("beforeend","<div style='height: 50px'></div>");
 
+const codeBlock = $$ (".code-block");
+
+const CodeFrame =  $$ ("code");
+
+var SaveCode = [];
+
+CodeFrame.forEach ((item) => {
+	SaveCode.push (item.textContent);
+	if (item.classList.contains ("language-css")) {
+		item.innerHTML = replaceAll (item.innerHTML,'[tab_js]','&emsp;&emsp;&emsp;&emsp; ');
+	}
+	else
+		item.innerHTML = replaceAll (item.innerHTML,'[tab_js]','&emsp;&emsp;&emsp;&emsp;');
+})
+
+codeBlock.forEach ((item) => {
+	item.insertAdjacentHTML ("beforebegin","<div class='code-toolbar'></div>");
+})
+
+
+const codeToolbar = $$ (".code-toolbar");
+
+codeToolbar.forEach ((item, index) => {
+	item.appendChild(codeBlock[index]);
+})
+
+codeBlock.forEach ((item) => {
+	item.insertAdjacentHTML ("beforebegin","<div class='pseudoMac'></div>");
+	item.insertAdjacentHTML ("afterbegin","<button class='copy'>Copy</button>");
+})
+
+const Copy = $$ (".copy");
+
 function copyFN (string) {
-	navigator.clipboard.writeText(string);
-	Swal.fire(
-		'COPY THÀNH CÔNG',
-		'',
-		'success'
-	);
+	navigator.clipboard.writeText(replaceAll (string,'[tab_js]','	'));
 }
 
+Copy.forEach ((item, index) => {
+	item.onclick = ()  => {
+		copyFN (SaveCode[index]);
+		item.textContent = "Copied";
+		setTimeout (() => {
+			item.textContent = "Copy";
+		},1000);
+	}
+})
 
+function replaceAll (text, search, replace) {
+    return text.split(search).join(replace);
+}
 //<div style="display: block;height: 125px;"></div>
